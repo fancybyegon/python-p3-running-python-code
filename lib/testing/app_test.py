e6ae51012a53d1a3f97cc2c7cfdc2d88386a68ea
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-05-23 08:53:15 +0300

    running tests in python

--- lib/testing/app_test.py
+++ lib/testing/app_test.py
@@ -1,1 +1,9 @@
-print("Hello World! Pass this test, please.")
+from os import path
+
+def test_app_py_exists():
+    assert path.exists("lib/app.py")
+
+def test_app_py_prints_expected_output():
+    import subprocess
+    result = subprocess.run(["python", "lib/app.py"], capture_output=True, text=True)
+    assert result.stdout.strip() == "Hello World! Pass this test, please."
